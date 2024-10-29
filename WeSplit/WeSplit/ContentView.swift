8d8ae9165d4507df07758ff8abacfea2fdbe0cce
Viktoriia <98652392+fullstack2002@users.noreply.github.com>
2024-10-29 19:51:59 +0100

    day 18 project 1

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,7 +8,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
@@ -23,6 +23,15 @@ struct ContentView: View {
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
+    }
+    
+    var totalCheckAmoount: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
     }
     
     var body: some View {
@@ -40,17 +49,36 @@ struct ContentView: View {
                 }
             }
             
-            Section("How much do you want to tip?") {
+//            Section(header: Text("How much do you want to tip?")) {
+//                Picker("Tip percentage", selection: $tipPercentage) {
+//                    ForEach(tipPercentages, id: \.self) {
+//                        Text($0, format: .percent)
+//                    }
+//                }
+//                .pickerStyle(.segmented)
+//            }
+//            challenge 3
+//            Change the tip percentage picker to show a new screen rather than using a segmented control, and give it a wider range of options – everything from 0% to 100%. Tip: use the range 0..<101 for your range rather than a fixed array.
+            Section(header: Text("How much do you want to tip?")) {
                 Picker("Tip percentage", selection: $tipPercentage) {
-                    ForEach(tipPercentages, id: \.self) {
-                        Text($0, format: .percent)
+                    ForEach(0..<101) {
+                        Text("\($0) %")
                     }
                 }
-                .pickerStyle(.segmented)
+                .pickerStyle(.navigationLink)
+                
             }
             
-            Section {
+//            challenge 1
+//            Add a header to the third section, saying “Amount per person”
+            Section(header: Text("Amount per person")) {
                 Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            
+//            challenge 2
+//            Add another section showing the total amount for the check – i.e., the original amount plus tip value, without dividing by the number of people.
+            Section(header: Text("Total amount for the check")) {
+                Text(totalCheckAmoount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
         .navigationTitle("WeSplit")
