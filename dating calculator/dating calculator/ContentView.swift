89f85af22564e4e6f805d35e9809a24bfa572636
Viktoriia <vika.vinnikova@gmail.com>
2025-04-27 23:57:38 +0200

    update

--- dating calculator/dating calculator/ContentView.swift
+++ dating calculator/dating calculator/ContentView.swift
@@ -8,18 +8,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let startDate = Calendar.current.date(from: DateComponents(year: 2025, month: 3, day: 25, hour: 22, minute: 30))!
+    let startDate = Calendar.current.date(from: DateComponents(year: 2025, month: 3, day: 25, hour: 22, minute: 30, second: 0))!
     
     var body: some View {
         let today = Date()
         
-        let components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: startDate, to: today)
+        let components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: startDate, to: today)
         
         let years = components.year ?? 0
         let months = components.month ?? 0
         let days = components.day ?? 0
         let hours = components.hour ?? 0
         let minutes = components.minute ?? 0
+        let seconds = components.second ?? 0
         
         VStack(spacing: 10) {
             Text("days together 💗")
@@ -46,6 +47,11 @@ struct ContentView: View {
             Text("\(minutes) minutes")
                     .font(.title2)
                     .bold()
+            
+            Text("\(seconds) seconds")
+                    .font(.title2)
+                    .bold()
+            
             Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
