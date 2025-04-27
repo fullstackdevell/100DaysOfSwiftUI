45b8bbf4ace1325a646dc65f1d28887115f5a64d
Viktoriia <vika.vinnikova@gmail.com>
2025-04-27 23:52:17 +0200

    new project

---
+++ dating calculator/dating calculator/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  dating calculator
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let startDate = Calendar.current.date(from: DateComponents(year: 2025, month: 3, day: 25, hour: 22, minute: 30))!
+    
+    var body: some View {
+        let today = Date()
+        
+        let components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: startDate, to: today)
+        
+        let years = components.year ?? 0
+        let months = components.month ?? 0
+        let days = components.day ?? 0
+        let hours = components.hour ?? 0
+        let minutes = components.minute ?? 0
+        
+        VStack(spacing: 10) {
+            Text("days together 💗")
+                .font(.largeTitle)
+                    .bold()
+                    .padding()
+            
+            Text("\(years) years")
+                    .font(.title2)
+                    .bold()
+                
+            Text("\(months) months")
+                    .font(.title2)
+                    .bold()
+                
+            Text("\(days) days")
+                    .font(.title2)
+                    .bold()
+            
+            Text("\(hours) hours")
+                    .font(.title2)
+                    .bold()
+            
+            Text("\(minutes) minutes")
+                    .font(.title2)
+                    .bold()
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    ContentView()
+}
+//22:30
---
+++ dating calculator/dating calculator/dating_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  dating_calculatorApp.swift
+//  dating calculator
+//
+//  Created by Viktoriia Vinnykova on 27.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct dating_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
