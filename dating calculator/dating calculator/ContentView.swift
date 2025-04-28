d1a1baa918f6e274e97fd55bf7dc3c8d33e2bb81
Viktoriia <vika.vinnikova@gmail.com>
2025-04-28 23:02:48 +0200

    update app

--- dating calculator/dating calculator/ContentView.swift
+++ dating calculator/dating calculator/ContentView.swift
@@ -10,55 +10,67 @@ struct ContentView: View {
     
     let startDate = Calendar.current.date(from: DateComponents(year: 2025, month: 3, day: 25, hour: 22, minute: 30, second: 0))!
     
+    @State private var now = Date()
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        let today = Date()
         
-        let components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: startDate, to: today)
+        let components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: startDate, to: now)
         
-        let years = components.year ?? 0
-        let months = components.month ?? 0
-        let days = components.day ?? 0
-        let hours = components.hour ?? 0
-        let minutes = components.minute ?? 0
-        let seconds = components.second ?? 0
-        
-        VStack(spacing: 10) {
-            Text("days together 💗")
-                .font(.largeTitle)
-                    .bold()
+        let timeData: [(String, Int)] = [
+                    ("Years", components.year ?? 0),
+                    ("Months", components.month ?? 0),
+                    ("Days", components.day ?? 0),
+                    ("Hours", components.hour ?? 0),
+                    ("Minutes", components.minute ?? 0),
+                    ("Seconds", components.second ?? 0)
+                ]
+   
+        ZStack {
+            
+                    VStack(spacing: 10) {
+                        
+                        HStack {
+                            Text("💗")
+                                .font(.system(size: 20, weight: .bold))
+                            
+                            Text("25.03.2025")
+                                .font(.system(size: 20, weight: .bold))
+                            
+                            Spacer()
+                        }
+                        .padding(20)
+                        
+                        Spacer()
+                        
+                        Text("days together💗")
+                            .font(.system(size: 58, weight: .bold))
+                            .padding(.bottom, 30)
+                        
+                        ForEach(timeData, id: \.0) { (label, value) in
+                                HStack(spacing: 10) {
+                                    Text("\(value)")
+                                        .font(.system(size: 38, weight: .bold))
+                                        .contentTransition(.numericText())
+                                        .animation(.easeInOut, value: value)
+
+                                    Text(label.lowercased())
+                                        .font(.system(size: 38, weight: .bold))
+                                }
+                            }
+                        
+                        Spacer()
+                    }
                     .padding()
-            
-            Text("\(years) years")
-                    .font(.title2)
-                    .bold()
-                
-            Text("\(months) months")
-                    .font(.title2)
-                    .bold()
-                
-            Text("\(days) days")
-                    .font(.title2)
-                    .bold()
-            
-            Text("\(hours) hours")
-                    .font(.title2)
-                    .bold()
-            
-            Text("\(minutes) minutes")
-                    .font(.title2)
-                    .bold()
-            
-            Text("\(seconds) seconds")
-                    .font(.title2)
-                    .bold()
-            
-            Spacer()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .onReceive(timer) { input in
+                    now = input
+                }
     }
 }
 
 #Preview {
     ContentView()
 }
-//22:30
