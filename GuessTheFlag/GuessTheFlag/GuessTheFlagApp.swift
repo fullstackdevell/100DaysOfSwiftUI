8f2d96ea73da3dd1c6b1274de04e0774b5b545e5
Viktoriia <vika.vinnikova@gmail.com>
2025-07-11 21:33:39 +0200

    GuessTgeFlag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Viktoriia Vinnykova on 11.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
